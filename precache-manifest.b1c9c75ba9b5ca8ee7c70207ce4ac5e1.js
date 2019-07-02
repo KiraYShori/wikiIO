116b15bf1a51500700b96f3486ed2dbff4179b55
priPCPhill <priscilla.rm.carmo@gmail.com>
2019-07-02 16:10:34 -0300

    Updates

---
+++ precache-manifest.b1c9c75ba9b5ca8ee7c70207ce4ac5e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "12610c6061cf8f8cd0a992738064c9ce",
+    "url": "/static/media/backWiki.12610c60.png"
+  },
+  {
+    "revision": "06b260167b96d5b56285",
+    "url": "/static/css/main.2e0cca60.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "98538cd07b841776efe7",
+    "url": "/static/js/2.37d84af5.chunk.js"
+  },
+  {
+    "revision": "83a9f52906f3c6aeb180b1faffe3fb0d",
+    "url": "/static/media/back1.83a9f529.png"
+  },
+  {
+    "revision": "33ddccee8fd141fbfd9a37aaa36899da",
+    "url": "/static/media/WikiOLAP.33ddccee.png"
+  },
+  {
+    "revision": "06b260167b96d5b56285",
+    "url": "/static/js/main.3dc47e44.chunk.js"
+  },
+  {
+    "revision": "f83059ff7b3d1ee34344e042d29cb3a7",
+    "url": "/static/media/backDA.f83059ff.png"
+  },
+  {
+    "revision": "46a677450eb2ea4c6271b7832cd18bd1",
+    "url": "/static/media/bdFundo5.46a67745.png"
+  },
+  {
+    "revision": "74972fc212be315dd53ad710c38a670f",
+    "url": "/static/media/balance-scale.74972fc2.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60b904bc6f102692b40e8bc9da9b9728",
+    "url": "/index.html"
+  }
+];
